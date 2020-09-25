1fd62de58dd9ffd55e8f778e2c11419ec1d934a9
randel <randelfreitas@gmail.com>
2020-09-25 10:14:02 -0300

    Updates

---
+++ precache-manifest.8585df4fe75652fc6479f2503dbdb636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca8dd559a28526848dbe325aadfdafa",
+    "url": "/RandelFreitas/react-nested/index.html"
+  },
+  {
+    "revision": "301dc63dd82cde12c384",
+    "url": "/RandelFreitas/react-nested/static/js/2.b81767d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RandelFreitas/react-nested/static/js/2.b81767d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "914f10da4bf2d266bef1",
+    "url": "/RandelFreitas/react-nested/static/js/main.458a974c.chunk.js"
+  },
+  {
+    "revision": "928579e5147981a19989",
+    "url": "/RandelFreitas/react-nested/static/js/runtime-main.9cfff11b.js"
+  }
+]);
