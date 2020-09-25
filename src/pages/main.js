3955d04fd0446c16d1ea1c8621081f8880973178
randel <randelfreitas@gmail.com>
2020-09-25 11:28:48 -0300

    Ajuste

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Link, useRouteMatch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import RoutesApp from '../pages/routesApp';
 
 const Main = () => {
-  const match = useRouteMatch();
-
   return (
     <div>
       <ul>
-        <li><Link to={`/component1`}>Component 1</Link></li>
-        <li><Link to={`/component2`}>Component 2</Link></li>
+        <li><Link to='/component1'>Component 1</Link></li>
+        <li><Link to='/component2'>Component 2</Link></li>
       </ul>
       <div>
         <RoutesApp />
--- src/pages/routesApp.js
+++ src/pages/routesApp.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Component1 from './aninhadas/component1';
 import Component2 from './aninhadas/component2';
 
 const RoutesApp = () => {
-  const match = useRouteMatch();
-  
   return (
     <Switch>
-      <Route exact path={`${match.url}`} component={()=><h1>padrao</h1>} />
-      <Route exact path={`${match.url}/component1`} component={Component1} />
-      <Route exact path={`${match.url}/component2`} component={Component2} />
+      <Route exact path='/' component={()=><h1>padrao</h1>} />
+      <Route exact path='/component1' component={Component1} />
+      <Route exact path='/component2' component={Component2} />
     </Switch>
   );
 }
